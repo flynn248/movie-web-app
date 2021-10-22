fe00d03a0fb04ca9de10dfd9251cdae89554d4b7
Shane Flynn <sflynn248@gmail.com>
2021-10-21 23:50:11 -0500

    init

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
-const info = require('./info.json');
 const app = express();
+
+app.set('view engine', 'pug')
 
 const PORT = process.env.PORT || 9090
 
 // Go to localhost:9090 in your browser while the program is running
 app.get('/', (req, res) => {
-    res.redirect(info.partOne + info.partTwo);
+    res.render('search.pug'); 
 });
 
+app.get('/Results', (req, res) => {
+  res.send("Result!")
+})
 // Starts an http server on the $PORT environment variable
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
