69e7a26edb828515f559c447f2808264444b7fb0
Shane Flynn <sflynn248@gmail.com>
2021-10-22 19:53:48 -0500

    add responsive dev feedback

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
+var livereload = require('livereload');
+var server = livereload.createServer();
+server.watch(__dirname + "/public");
+
+// automatically refresh home page
+server.server.once("connection", () => {
+  setTimeout(() => {
+    server.refresh("/")
+  }, 100);
+});
+
+var connectLiveReload = require("connect-livereload")
+
 const express = require('express');
 const app = express();
+const path = require('path')
 
+app.use(connectLiveReload())
+
+// view engine setup
 app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 9090
 
 // Go to localhost:9090 in your browser while the program is running
 app.get('/', (req, res) => {
-    res.render('search.pug'); 
+  res.render('search.pug'); 
 });
 
 app.get('/Results', (req, res) => {
-  res.send("Result!")
+  res.render('result.pug')
 })
 // Starts an http server on the $PORT environment variable
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
 });
+
