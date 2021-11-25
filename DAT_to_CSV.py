243f870d9f4ced621b8bba660db4d2d44d2a0689
Shane Flynn <sflynn248@gmail.com>
2021-11-25 16:25:45 -0600

    make csv dir

--- DAT_to_CSV.py
+++ DAT_to_CSV.py
@@ -3,24 +3,34 @@
 import os
 import csv
 
-FILE_PATH = "C:\\Users\\sflyn\\Documents\\UW-Whitewater\\Fall 2021\\Database Mgmt Systems\\Movie Database"
+FILE_PATH = "C:\\Users\\Shane\\Documents\\UW-Whitewater\\Fall 2021\\Movie Data"
 FILE_NAME = "movies.dat"
 
 def datFilePath() -> tuple[str, str]:
     filepath = input("Enter the directory path of the .dat file you'd like to convert: ")
     fileName = input("Enter the .dat file name: ")
-    if os.path.exists(filepath + "\\" + fileName):
+    path = os.path.join(filepath, fileName)
+    if os.path.exists(path):
         return filepath, fileName
     else:
-        print(f"${filepath}\{fileName} does not exist!!")
+        print(f"${path} does not exist!!")
         raise FileNotFoundError
+
+def createCSVDir(filePath: str):
+    csvDir = "Database CSV"
+    path = os.path.join(filePath, csvDir)
+    if not os.path.exists(path):
+        os.mkdir(path)
 
 def convertToCsv(datFilePath: str, datFileName: str):
     delimeter = '\n'
     separator = '\t'
     csvFileName = datFileName[:-3] + "csv"
+    datPath = os.path.join(datFilePath, datFileName)
+    
+    createCSVDir(datFilePath)
 
-    with open(datFilePath + '\\' + datFileName) as f:
+    with open(datPath) as f:
         next(f) # Skip header line
         with open(datFilePath + '\\' + csvFileName, 'w', newline='') as csvFile:
             dataWriter = csv.writer(csvFile)
@@ -39,7 +49,7 @@ def main():
 #        filePath = datFilePath()
 #    except Exception:
 #        print(f"Failed to find")
-    #convertToCsv(FILE_PATH, FILE_NAME)
+    convertToCsv(FILE_PATH, FILE_NAME)
     
     print("cake")
 
