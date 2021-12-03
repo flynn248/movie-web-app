03e4d80c6d982eb54a36fff061134c8a10fc38bc
AFilippini <annafilippini@att.net>
2021-12-03 14:58:41 -0600

    ...

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ server.server.once("connection", () => {
 
 var connectLiveReload = require("connect-livereload")
 
-const Search = require(__dirname + '/functions/search.js')
+//const Search = require(__dirname + '/functions/search.js')
 const express = require('express');
 const app = express();
 const path = require('path')
