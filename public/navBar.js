51a1a74dcf253bde99aa2ceb94adccaf2fdf0ac8
Shane Flynn <sflynn248@gmail.com>
2021-12-05 14:08:27 -0600

    change stuff.js to navBar.js

---
+++ public/navBar.js
@@ -0,0 +1,20 @@
+//This file can be used for normal JavaScript stuff
+
+var $ = id => {return document.getElementById(id);};
+
+const USERNAME = Result.userName;
+headLinks = $('link')
+console.log(headLinks)
+const countLinks = () => {
+    links = headLinks.getElementsByTagName('a').length
+    return links
+}
+
+if(countLinks() > 1 && USERNAME === undefined){
+    headLinks.lastChild.remove();
+} else if(countLinks() == 1 && USERNAME !== undefined){
+    newA = document.createElement('a');
+    newA.setAttribute('href', `/user/${USERNAME}`);
+    newA.setAttribute('id', 'user-link');
+    headLinks.appendChild(newA).innerText = USERNAME;
+}
