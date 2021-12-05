992a6ab08ad1755fcb8669d10084f82c7f82d989
Shane Flynn <sflynn248@gmail.com>
2021-12-05 14:08:27 -0600

    change login.js to user.js

--- functions/user.js
+++ functions/user.js
@@ -1,10 +1,10 @@
 const database = require('./database.js')
 const db = database.db
 
-const addNewUser = async (UN, PWD) => {
+const addNewUser = (UN, PWD) => {
     return new Promise((resolve, reject) => {
         sql = `
-        INSERT INTO User VALUES('${UN}', '${PWD}')
+        INSERT INTO User VALUES(${db.escape(UN)}, ${db.escape(PWD)})
         `
         db.query(sql, (e, result) => {
             if(e) 
@@ -14,8 +14,24 @@ const addNewUser = async (UN, PWD) => {
     })
 }
 
-const removeUser = async (UN) => {
+const removeUser = (UN) => {
     return new Promise((resolve, reject) => {
+        sql =  `
+        DELETE FROM HaveCom WHERE commentID IN (SELECT DISTINCT c.commentID FROM MakeCom c WHERE c.userName LIKE '${UN}')
+        `
+        db.query(sql, (e, result) => {
+            if(e) 
+                throw e
+        })
+
+        sql =  `
+        DELETE FROM Comment WHERE commentID IN (SELECT DISTINCT c.commentID FROM MakeCom c WHERE c.userName LIKE '${UN}')
+        `
+        db.query(sql, (e, result) => {
+            if(e) 
+                throw e
+        })
+
         sql = `
         DELETE FROM User WHERE userName LIKE '${UN}'
         `
@@ -32,6 +48,7 @@ const removeUser = async (UN) => {
                 throw e
         })
         
+        
         sql = `
         DELETE FROM MakeCom WHERE userName LIKE '${UN}'
         `
@@ -40,6 +57,31 @@ const removeUser = async (UN) => {
                 throw e
         })
 
+        sql =  `
+        DELETE FROM HaveRating 
+        WHERE ratingID IN (
+            SELECT DISTINCT hr.ratingID 
+            FROM HaveRating AS hr 
+            LEFT OUTER JOIN UserRating AS ur ON ur.ratingID = hr.ratingID 
+            LEFT OUTER JOIN MakeRating mr ON mr.ratingID = ur.ratingID 
+            WHERE mr.userName LIKE '${UN}')
+        `
+        db.query(sql, (e, result) => {
+            if(e) 
+                throw e
+        })
+
+        sql =  `
+        DELETE FROM UserRating 
+        WHERE ratingID IN (
+            SELECT DISTINCT mr.ratingID 
+            FROM MakeRating AS mr 
+            WHERE mr.userName LIKE '${UN}')
+        `
+        db.query(sql, (e, result) => {
+            if(e) 
+                throw e
+        })
 
         sql = `
         DELETE FROM MakeRating WHERE userName LIKE '${UN}'
@@ -53,12 +95,12 @@ const removeUser = async (UN) => {
     })
 }
 
-const checkIfUserExists = async (UN) => {
+const checkIfUserExists = (UN) => {
     return new Promise((resolve, reject) => {
         sql = `
         SELECT COUNT(DISTINCT userName) AS count
         FROM User
-        WHERE userName = '${UN}'
+        WHERE userName LIKE ${db.escape(UN)}
         `
         
         const exists = new Promise((rslv, rjct) => db.query(sql, (e, result) => {
@@ -77,12 +119,12 @@ const checkIfUserExists = async (UN) => {
 }
 
 // Verify the input of the Username and Password
-const verifyUserLogin = async (UN, PWD) => {
+const verifyUserLogin = (UN, PWD) => {
     return new Promise((resolve, reject) => {
         sql = `
         SELECT COUNT(DISTINCT userName) AS count
         FROM User
-        WHERE userName = '${UN}' AND pwd = '${PWD}'
+        WHERE userName LIKE ${db.escape(UN)} AND pwd LIKE ${db.escape(PWD)}
         `
         
         const exists = new Promise((rslv, rjct) => db.query(sql, (e, result) => {
@@ -104,16 +146,17 @@ const verifyUserLogin = async (UN, PWD) => {
         -Fav movies
         -Comments made on movies
 */
-const getUserProfile = async uN => {
+const getUserProfile = uN => {
+    uN = db.escape(uN);
     return new Promise((resolve, reject) => {
         sql = `
         SELECT DISTINCT u.userName, 
                         f.movieID AS favMovieID, 
                         m.title
         FROM User AS u
-        LEFT OUTER JOIN Favorites AS f ON f.userName = '${uN}'
+        LEFT OUTER JOIN Favorites AS f ON f.userName LIKE ${uN}
         LEFT OUTER JOIN Movie AS m ON f.movieID = m.movieID
-        WHERE u.userName = '${uN}'
+        WHERE u.userName LIKE ${uN}
         `
         const userFavMovies = new Promise((rslv, rjct) => db.query(sql, (e, result) => {
             if (e) 
@@ -122,7 +165,7 @@ const getUserProfile = async uN => {
             if(result.length != 0){
                 obj = {
                     userName: result[0].userName
-                }
+                } 
                 favMovie = []
 
                 result.forEach(element => {
@@ -156,12 +199,12 @@ const getUserProfile = async uN => {
     })
 }
 
-const queryComments = async (uN, userDetails) => {
+const queryComments = (uN, userDetails) => {
     sql = `
     SELECT DISTINCT c.commentID, c.txt, 
                     hC.movieID AS comMovieID
     FROM User AS u
-    LEFT OUTER JOIN MakeCom AS mC ON mC.userName = '${uN}'
+    LEFT OUTER JOIN MakeCom AS mC ON mC.userName LIKE ${uN}
     LEFT OUTER JOIN Comment AS c ON mC.commentID = c.commentID
     LEFT OUTER JOIN HaveCom AS hC ON c.commentID = hC.commentID
     `
@@ -199,10 +242,58 @@ const queryComments = async (uN, userDetails) => {
     }))
 }
 
+const alreadyFavorited = async (movieID, UN) => {
+    return new Promise((resolve, reject) => {
+        sql = `
+        SELECT COUNT(movieID) AS count
+        FROM Favorites
+        WHERE movieID = ${movieID} AND userName LIKE '${UN}'
+        `
+        const exists = new Promise((rslv, rjct) => db.query(sql, (e, result) => {
+            if(e) 
+                throw e
+            count = result[0].count
+            if(count == 0)
+                rslv(false)
+            else if(count == 1)
+                rslv(true)
+            else
+                rjct(undefined)
+        }))
+        resolve(exists)
+    })
+}
+
+const alreadyRated = (movieID, UN) => {
+    return new Promise((resolve, reject) => {
+        sql = `
+        SELECT COUNT(DISTINCT hr.movieID) AS count
+        FROM HaveRating hr
+        LEFT OUTER JOIN UserRating AS ur ON ur.ratingID = hr.ratingID
+        LEFT OUTER JOIN MakeRating AS mr ON mr.ratingID = ur.ratingID
+        WHERE hr.movieID = ${movieID} AND mr.userName LIKE '${UN}'
+        `
+        const exists = new Promise((rslv, rjct) => db.query(sql, (e, result) => {
+            if(e) 
+                throw e
+            count = result[0].count
+            if(count == 0)
+                rslv(false)
+            else if(count == 1)
+                rslv(true)
+            else
+                rjct(undefined)
+        }))
+        resolve(exists)
+    })
+}
+
 module.exports = {
     getUserProfile: getUserProfile,
     verifyUserLogin: verifyUserLogin,
     checkIfUserExists: checkIfUserExists,
     addNewUser: addNewUser,
-    removeUser: removeUser
+    removeUser: removeUser,
+    alreadyFavorited: alreadyFavorited,
+    alreadyRated: alreadyRated
 }
