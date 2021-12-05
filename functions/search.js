42bc992027875bfea3a2bbdb0dae1091bcb2ed00
Shane Flynn <sflynn248@gmail.com>
2021-12-05 14:08:27 -0600

    separate location search into separate queries

--- functions/search.js
+++ functions/search.js
@@ -11,8 +11,16 @@ const getMovieFromSearch = (type, param) => {
             sql = `SELECT DISTINCT m.movieID, m.title FROM Movie m, Director d, Directed dJ WHERE d.dName LIKE '%${param}%' AND d.directorID = dJ.directorID AND dJ.movieID = m.movieID GROUP BY m.title ORDER BY m.title`
         else if (type == 'actor')
             sql = `SELECT DISTINCT m.movieID, m.title FROM Movie m, Actor a, Acted aJ WHERE a.aName LIKE '%${param}%' AND a.actorID = aJ.actorID AND aJ.movieID = m.movieID GROUP BY m.title ORDER BY m.title`
-        else if (type == 'location') 
-            sql = `SELECT DISTINCT m.movieID, m.title FROM Movie m, Location l, Filmed f WHERE l.country LIKE '%${param}%' OR l.state LIKE '%${param}%' OR l.city LIKE '%${param}%' OR l.location LIKE '%${param}%' AND l.locationID = f.locationID AND f.movieID = m.movieID GROUP BY m.title ORDER BY m.title`
+        else if (type == 'released_country')
+            sql = `SELECT DISTINCT movieID, title FROM Movie WHERE country LIKE '%${param}%'`
+        else if (type == 'location_country')
+            sql = `SELECT DISTINCT m.movieID, m.title FROM Movie m, Location l, Filmed f WHERE l.country LIKE '%${param}%' AND l.locationID = f.locationID AND f.movieID = m.movieID GROUP BY m.title ORDER BY m.title`
+        else if (type == 'location_state')
+            sql = `SELECT DISTINCT m.movieID, m.title FROM Movie m, Location l, Filmed f WHERE l.state LIKE '%${param}%' AND l.locationID = f.locationID AND f.movieID = m.movieID GROUP BY m.title ORDER BY m.title`
+        else if (type == 'location_city')
+            sql = `SELECT DISTINCT m.movieID, m.title FROM Movie m, Location l, Filmed f WHERE l.city LIKE '%${param}%' AND l.locationID = f.locationID AND f.movieID = m.movieID GROUP BY m.title ORDER BY m.title`
+        else if (type == 'location_local')
+            sql = `SELECT DISTINCT m.movieID, m.title FROM Movie m, Location l, Filmed f WHERE l.location LIKE '%${param}%' AND l.locationID = f.locationID AND f.movieID = m.movieID GROUP BY m.title ORDER BY m.title`
         else if (type == 'rating_gt')
             sql = `SELECT DISTINCT movieID, title FROM Movie WHERE (rtAudienceRating * 2 + rtTopCriticsRating + rtAllCriticsRating)/3 >= ${param} GROUP BY title ORDER BY title`
         else if (type == 'rating_ls')
@@ -54,17 +62,15 @@ const getMovieDetails = (ID) => {
         sql = `
         SELECT DISTINCT m.movieID, m.title, m.imbdID, m.year, m.country, m.rtID, m.rtAllCriticsRating, m.rtAllCriticsNumReviews, 
         m.rtTopCriticsRating, m.rtTopCriticsNumReviews, m.rtAudienceRating, m.rtAudienceNumReviews, m.rtPicURL,
-        a.aName, d.dName, c.txt, u.rating, g.genre, g.description,
+        a.aName, d.dName, c.txt, g.genre, g.description,
         l.country AS locCountry, l.state, l.city, l.location
         FROM Movie AS m
-        INNER JOIN Acted AS aJ ON aJ.movieID = ${ID}
-        INNER JOIN Actor AS a ON aJ.actorID = a.actorID
+        LEFT OUTER JOIN Acted AS aJ ON aJ.movieID = ${ID}
+        LEFT OUTER JOIN Actor AS a ON aJ.actorID = a.actorID
         LEFT OUTER JOIN Directed AS dJ ON dJ.movieID = ${ID}
         LEFT OUTER JOIN Director AS d ON dJ.directorID = d.directorID
         LEFT OUTER JOIN HaveCom AS cJ ON cJ.movieID = ${ID}
         LEFT OUTER JOIN Comment AS c ON cJ.commentID = c.commentID
-        LEFT OUTER JOIN HaveRating AS uJ ON uJ.movieID = ${ID}
-        LEFT OUTER JOIN UserRating AS u ON uJ.ratingID = u.ratingID
         LEFT OUTER JOIN Filmed AS lJ ON lJ.movieID = ${ID}
         LEFT OUTER JOIN Location AS l ON lJ.locationID = l.locationID
         LEFT OUTER JOIN Classified AS gJ ON gJ.movieID = ${ID}
@@ -73,10 +79,7 @@ const getMovieDetails = (ID) => {
         `
         const movie = new Promise((rslv, rjct) => db.query(sql, (e, result) => {
             if (e) 
-                throw e
-            console.log(result[0])
-            console.log()
-            console.log()
+            throw e
             if(result.length != 0){
                 obj = {
                     movieID: result[0].movieID,
@@ -92,34 +95,39 @@ const getMovieDetails = (ID) => {
                     rtAudienceRating: result[0].rtAudienceRating,
                     rtAudienceNumReviews: result[0].rtAudienceNumReviews,
                     rtPicURL: result[0].rtPicURL,
-                    dName: result[0].dName,
-                    locCountry: result[0].locCountry,
-                    locState: result[0].state,
-                    locCity: result[0].city,
-                    locLocal: result[0].location
+                    dName: result[0].dName
                 }
-
+                
                 actors = []
                 genres = []
                 comments = []
-                ratings = []
-
+                shotIn = []
+                
                 result.forEach(element => {
                     if(!actors.includes(element.aName))
-                        actors.push(element.aName)
+                    actors.push(element.aName)
                     if(!genres.includes(element.genre))
-                        genres.push(element.genre)
+                    genres.push(element.genre)
                     if(!comments.includes(element.txt))
-                        comments.push(element.txt)
-                    if(!ratings.includes(element.rating))
-                        ratings.push(element.rating)
+                    comments.push(element.txt)
+                    
+                    tmp = {
+                        locCountry: element.locCountry,
+                        locState: element.state,
+                        locCity: element.city,
+                        locLocal: element.location
+                    }
+                    
+                    if(!shotIn.some(e => e.locCountry == element.locCountry && e.locState == element.state && e.locCity == element.city && e.locLocal == element.location)){
+                        shotIn.push(tmp)
+                    }
                 })
-
-                obj['actors'] = actors
-                obj['genres'] = genres
-                obj['comments'] = comments
-                obj['ratings'] = ratings
-                rslv(obj)
+                obj['actors'] = actors;
+                obj['genres'] = genres;
+                obj['comments'] = comments;
+                obj['shotIn'] = shotIn;
+                
+                rslv(obj);
             }
             obj = {
                 movieID: null,
@@ -136,21 +144,39 @@ const getMovieDetails = (ID) => {
                 rtAudienceNumReviews: null,
                 rtPicURL: '',
                 dName: '',
-                locCountry: '',
-                locState: '',
-                locCity: '',
-                locLocal: '',
+                shotIn: [],
                 actors: [],
                 genres: [],
                 comments: [],
-                ratings: []
             }
             rjct(obj)
-        }))
+        })).then(movieDetails => {
+            sql = `
+            SELECT u.rating
+            FROM UserRating AS u
+            INNER JOIN HaveRating AS hr ON hr.ratingID = u.ratingID
+            WHERE hr.movieID = ${ID}
+            `
+            
+            return new Promise((rslv, rjct) => db.query(sql, (e, ratings) =>{
+                if(e) throw e;
+                movieDetails['userRating'] = null;
+                if(ratings.length != 0){                    
+                    var userRatingTotal = 0
+                    ratings.forEach(element => {
+                        userRatingTotal += element.rating
+                    })
+                    movieDetails.userRating = userRatingTotal / ratings.length;
+                    rslv(movieDetails);
+                }else{
+                    rslv(movieDetails);
+                }
+            }))
+        })
         resolve(movie)
     })
 }
-    
+
 module.exports = {
     getMovieFromSearch: getMovieFromSearch,
     getMovieDetails: getMovieDetails
