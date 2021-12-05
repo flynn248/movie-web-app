f6ad7a6f052a39d9ee893e9abd5282b372474ca6
Shane Flynn <sflynn248@gmail.com>
2021-12-05 14:12:50 -0600

    fix rating query and location query

--- functions/movieFunctions.js
+++ functions/movieFunctions.js
@@ -5,7 +5,27 @@ const db = database.db
 const favoriteMovie = (movieID, userName) => {
     return new Promise((resolve,reject) => {
         sql = `
-        INSERT INTO Favorites VALUES(${movieID}, ${userName})
+        INSERT INTO Favorites VALUES(${movieID}, '${userName}')
+        `
+        try{
+            db.query(sql, (e, result) => {
+                if(e) 
+                    throw e
+                resolve(true)
+            });
+        }
+        catch{
+            reject(false)
+        }
+    }).catch((e)=>{
+        console.log(e)
+    })
+}
+
+const removeFavoriteMovie = (movieID, userName) => {
+    return new Promise((resolve,reject) => {
+        sql = `
+        DELETE FROM Favorites WHERE movieID = ${movieID} AND userName LIKE '${userName}'
         `
         try{
             db.query(sql, (e, result) => {
@@ -24,47 +44,109 @@ const favoriteMovie = (movieID, userName) => {
 
 const makeComment = (userName, movieID, txt) => {
     return new Promise((resolve, reject) => {
-        commentCount = SP.getMovieCount().then((count) => {
-            return count
+        SP.getMaxCommentID().then((commentID) => {
+            sql = `
+            INSERT INTO Comment VALUES(${commentID + 1}, ${db.escape(txt)})
+            `
+            try{
+                db.query(sql, (e, result) => {if(e) throw e})
+            }
+            catch{
+                reject(false)
+            }
+            
+            sql = `
+            INSERT INTO MakeCom VALUES('${userName}', ${commentID + 1})
+            `
+            try{
+                db.query(sql, (e, result) => {if(e) throw e})
+            }
+            catch{
+                reject(false)
+            }
+            
+            sql = `
+            INSERT INTO HaveCom VALUES(${movieID}, ${commentID + 1})
+            `
+            try{
+                db.query(sql, (e, result) => {if(e) throw e})
+            }
+            catch{
+                reject(false)
+            }
+            
+            resolve(true)
         });
-        console.log(`I awaited for ${commentCount}`)
-        sql = `
-        INSERT INTO Comment VALUES(${commentCount + 1}, ${txt})
-        `
-        try{
-            db.query(sql, (e, result) => {if(e) throw e})
-        }
-        catch{
-            reject(false)
-        }
-
-        sql = `
-        INSERT INTO MakeCom VALUES(${userName}, ${commentCount + 1})
-        `
-        try{
-            db.query(sql, (e, result) => {if(e) throw e})
-        }
-        catch{
-            reject(false)
-        }
-
-        sql = `
-        INSERT INTO HaveCom VALUES(${movieID}, ${commentCount + 1})
-        `
-        try{
-            db.query(sql, (e, result) => {if(e) throw e})
-        }
-        catch{
-            reject(false)
-        }
-        
-        resolve(true)
-    }).catch((e)=>{
-        console.log(`ERROR IN makeComment ${e}`)
+        }).catch((e)=>{
+            console.log(`ERROR IN makeComment ${e}`)
     })
 }
-    
+
+const rateMovie = (movieID, userName, rating) => {
+    return new Promise((resolve, reject) => {
+        SP.getMaxRatingID().then((ratingID) => {
+            sql = `
+            INSERT INTO UserRating VALUES(${ratingID + 1}, ${rating})
+            `
+            try{
+                db.query(sql, (e, result) => {if(e) throw e})
+            }
+            catch{
+                reject(false)
+            }
+            
+            sql = `
+            INSERT INTO MakeRating VALUES('${userName}', ${ratingID + 1})
+            `
+            try{
+                db.query(sql, (e, result) => {if(e) throw e})
+            }
+            catch{
+                reject(false)
+            }
+            
+            sql = `
+            INSERT INTO HaveRating VALUES(${movieID}, ${ratingID + 1})
+            `
+            try{
+                db.query(sql, (e, result) => {if(e) throw e})
+            }
+            catch{
+                reject(false)
+            }
+            
+            resolve(true)
+        });
+        }).catch((e)=>{
+            console.log(`ERROR IN makeComment ${e}`)
+    })
+}
+
+const updateRating = (movieID, userName, rating) => {
+    return new Promise((resolve, reject) => {
+        sql = `
+        UPDATE UserRating
+        SET rating=${rating}
+        WHERE ratingID IN (
+            SELECT DISTINCT hr.ratingID 
+            FROM HaveRating AS hr 
+            LEFT OUTER JOIN UserRating AS ur ON ur.ratingID = hr.ratingID 
+            LEFT OUTER JOIN MakeRating mr ON mr.ratingID = ur.ratingID 
+            WHERE mr.userName LIKE '${userName}' AND hr.movieID = ${movieID}
+        )
+        `
+        db.query(sql, (e, result) => {
+            if(e) 
+                throw e
+        })
+        resolve(true);
+    })
+}
+
 module.exports = {
     favoriteMovie: favoriteMovie,
-    makeComment: makeComment
+    makeComment: makeComment,
+    removeFavoriteMovie: removeFavoriteMovie,
+    rateMovie: rateMovie,
+    updateRating: updateRating
 }
